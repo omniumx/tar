1fa698f2c04afc73082fa90023a187dc09fe7b42
leoab2 <75602757+leoab2@users.noreply.github.com>
2021-04-11 13:01:01 +0530

    Update and rename .constants.js.example to .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -3,10 +3,10 @@ module.exports = Object.freeze({
   ARIA_SECRET: 'aria2_secret',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
-  ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
+  ARIA_FILTERED_DOMAINS: ['yts'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: 'id_of_Drive_folder_to_upload_into',
+  GDRIVE_PARENT_DIR_ID: '0AB_OX_4rNMd-Uk9PVA',
   OTHER_GDRIVE_DIR_IDS: [], // This is needed if u want to look for files in multiple dirs on list command
   SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
