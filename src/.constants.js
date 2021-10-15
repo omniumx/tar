3b4e13f2c0e82467ebb4b9fe5ffbc197d588dfd3
omniumx <92170278+omniumx@users.noreply.github.com>
2021-10-15 17:13:27 +0800

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,31 +1,30 @@
 module.exports = Object.freeze({
-  TOKEN: 'bot_token',
-  ARIA_SECRET: 'aria2_secret',
-  ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
+  TOKEN: '2006627581:AAFhZYx2OD7Q-PZ_XeB8sjCPQ-Yt472xhPY',
+  ARIA_SECRET: 'stazholicindustries42069',
+  ARIA_DOWNLOAD_LOCATION: '/downloads',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
-  ARIA_FILTERED_DOMAINS: ['yts'], // Prevent downloading from URLs containing these substrings
+  ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: '0AB_OX_4rNMd-Uk9PVA',
-  OTHER_GDRIVE_DIR_IDS: [], // This is needed if u want to look for files in multiple dirs on list command
-  SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
-  STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
+  GDRIVE_PARENT_DIR_ID: '1z_K2WUF2sHpnN1jTY1pIqPfUAeCUx72P',
+  SUDO_USERS: [1997340590, 912107101],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [-1001220179392],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  STATUS_UPDATE_INTERVAL_MS: 5000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
     EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
   },
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
-    enabled: false,   // Set this to true to use the notify functionality
+    enabled: True,   // Set this to true to use the notify functionality
     host: 'hostname.domain',
     port: 80,
     path: '/botNotify'
   },
   COMMANDS_USE_BOT_NAME: {
-    ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
-    NAME: "@nameOf_bot"
+    ENABLED: True,  // If true, all commands except '/list' has to have the bot username after the command
+    NAME: "@mommyboa_bot"
   },
-  IS_TEAM_DRIVE: false,
-  USE_SERVICE_ACCOUNT: false,
+  IS_TEAM_DRIVE: True,
+  USE_SERVICE_ACCOUNT: True,
   INDEX_DOMAIN: "" // add an extra / at the end, for example https://www.gdindex.url/
 });
